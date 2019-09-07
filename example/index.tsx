9413119e659286312b0499af803cfd8845b1d1c0
nerdyman <averynerdyman@gmail.com>
2019-09-07 21:20:38 +0100

    feat: support portrait mode

--- example/index.tsx
+++ example/index.tsx
@@ -9,31 +9,57 @@ const Img: React.FC<any> = ({ style, ...props }) => (
 
 const App = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        width: '100%',
-        height: '100vh',
-        overflow: 'hidden',
-      }}
-    >
-      <ReactCompareSlider
-        // portrait
-        itemFirst={
-          <Img
-            src="https://images.pexels.com/photos/2040626/pexels-photo-2040626.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-            style={{ filter: 'blur(25px)' }}
-            alt="one"
-          />
-        }
-        itemSecond={
-          <Img
-            src="https://images.pexels.com/photos/2040626/pexels-photo-2040626.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-            alt="two"
-          />
-        }
-      />
-    </div>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          height: '50vh',
+          overflow: 'hidden',
+        }}
+      >
+        <ReactCompareSlider
+          portrait
+          itemFirst={
+            <Img
+              src="https://images.pexels.com/photos/2040626/pexels-photo-2040626.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              style={{ filter: 'blur(25px)' }}
+              alt="one"
+            />
+          }
+          itemSecond={
+            <Img
+              src="https://images.pexels.com/photos/2040626/pexels-photo-2040626.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              alt="two"
+            />
+          }
+        />
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          height: '50vh',
+          overflow: 'hidden',
+        }}
+      >
+        <ReactCompareSlider
+          itemFirst={
+            <Img
+              src="https://images.pexels.com/photos/2040626/pexels-photo-2040626.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              style={{ filter: 'blur(25px)' }}
+              alt="one"
+            />
+          }
+          itemSecond={
+            <Img
+              src="https://images.pexels.com/photos/2040626/pexels-photo-2040626.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              alt="two"
+            />
+          }
+        />
+      </div>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ export const CLIENT_SUPPORTS_CSS_CLIP_PATH =
   CSS.supports &&
   CSS.supports('clip-path', 'inset(0 0 0 0)');
 
+/**
+ * Permitted props for `styleFitContainer`
+ */
 interface StyleFitContainerProps {
   objectFit?: React.CSSProperties['objectFit'];
 }
 
 /**
- * Style object util to fit container
+ * Style CSS util to fit container
  */
 export const styleFitContainer = ({
   objectFit = 'cover',
@@ -25,6 +28,9 @@ export const styleFitContainer = ({
 
 type ReactCompareSliderPropPosition = number;
 
+/**
+ * Common props shared between child components
+ */
 interface ReactCompareSliderCommonProps {
   portrait?: boolean;
   position: ReactCompareSliderPropPosition;
@@ -39,11 +45,12 @@ const ReactCompareSliderHandleContainer: React.FC<
   const style: React.CSSProperties = {
     position: 'absolute',
     top: 0,
-    width: portrait ? '100%' : 'auto',
-    height: portrait ? 'auto' : '100%',
+    width: '100%',
+    height: '100%',
+    pointerEvents: 'none',
     transform: portrait
-      ? `translateY(${position}px)`
-      : `translateX(${position}px)`,
+      ? `translateY(${position}%)`
+      : `translateX(${position}%)`,
   };
 
   return <div style={style}>{children}</div>;
@@ -63,16 +70,22 @@ export const ReactCompareSliderHandle: React.FC<
     transform: portrait ? 'translateY(-50%)' : 'translateX(-50%)',
     backgroundColor: '#ffffff',
     boxShadow: '0 0 .2rem #000000',
-    cursor: 'ew-resize',
+    cursor: portrait ? 'ns-resize' : 'ew-resize',
+    pointerEvents: 'all',
   };
 
   return <div {...props} style={style} />;
 };
 
+interface ReactCompareSliderItemProps extends ReactCompareSliderCommonProps {
+  positionPx: number;
+}
+
 /**
  * Container for items passed to main component
  */
-const ReactCompareSliderItem: React.FC<ReactCompareSliderCommonProps> = ({
+const ReactCompareSliderItem: React.FC<ReactCompareSliderItemProps> = ({
+  positionPx,
   portrait,
   position,
   ...props
@@ -83,12 +96,16 @@ const ReactCompareSliderItem: React.FC<ReactCompareSliderCommonProps> = ({
     left: 0,
     width: '100%',
     height: '100%',
-    clipPath: `inset(0 0 ${portrait ? `${position}px 0` : `0 ${position}px`})`,
+    clipPath: portrait
+      ? `inset(${position}% 0 0 0`
+      : `inset(0 0 0 ${position}%`,
   };
 
   // Use `clip` if `clip-path` is not supported
   if (!CLIENT_SUPPORTS_CSS_CLIP_PATH) {
-    style.clip = `rect(auto,${position}px,auto,auto)`;
+    style.clip = portrait
+      ? `rect(auto,auto,${positionPx}px,auto)`
+      : `rect(auto,${positionPx}px,auto,auto)`;
   }
 
   return <div {...props} style={style} />;
@@ -103,8 +120,15 @@ export interface ReactCompareSliderProps {
   itemSecond: React.ReactNode;
   /** Orientation */
   portrait?: boolean;
-  /** Position of divide */
+  /** Percentage position of divide */
   position?: ReactCompareSliderPropPosition;
+}
+
+interface ReactCompareSliderStatePositions {
+  /** Position percentage */
+  position: number;
+  /** Position in px for legacy browsers */
+  positionPx: number;
 }
 
 /**
@@ -115,52 +139,73 @@ export const ReactCompareSlider: React.FC<ReactCompareSliderProps> = ({
   itemFirst,
   itemSecond,
   portrait,
-  position = window.innerWidth / 2,
+  position = 50,
   ...props
 }): React.ReactElement => {
   const containerRef = useRef<HTMLDivElement>(document.createElement('div'));
   const [isDragging, setIsDragging] = useState(false);
-  const [internalPosition, setInternalPosition] = useState(position);
+  const [positions, setPositions] = useState<ReactCompareSliderStatePositions>({
+    position,
+    positionPx: 0,
+  });
+
+  // Set initial position for legacy browsers
+  useEffect((): void => {
+    if (!CLIENT_SUPPORTS_CSS_CLIP_PATH) {
+      setPositions(positions => {
+        const { width, height } = containerRef.current.getBoundingClientRect();
+        console.log({ width, height });
+        return {
+          ...positions,
+          positionPx: portrait
+            ? (height * position) / 100
+            : (width * position) / 100,
+        };
+      });
+    }
+  }, []);
 
   // Update internal position if position prop changes
   useEffect((): void => {
-    setInternalPosition(position);
+    setPositions(positions => ({ ...positions, position }));
   }, [position]);
 
+  // Bind and handle events
   useEffect((): (() => void) => {
-    const updatePosition = (position: number): void => {
-      setInternalPosition(
-        position +
-          (portrait
-            ? containerRef.current.clientTop
-            : containerRef.current.clientLeft)
-      );
+    const updatePosition = (x: number, y: number): void => {
+      const { width, height } = containerRef.current.getBoundingClientRect();
+
+      setPositions(positions => ({
+        ...positions,
+        positionPx: portrait ? y : x,
+        position: portrait ? (y / height) * 100 : (x / width) * 100,
+      }));
     };
 
     const handleMouseDown = (ev: MouseEvent): void => {
       if (!isDragging) setIsDragging(true);
-      updatePosition(ev.clientX);
+      updatePosition(ev.pageX, ev.pageY);
     };
 
     const handleMouseMove = (ev: MouseEvent): void => {
       ev.preventDefault();
       if (isDragging) {
         requestAnimationFrame((): void => {
-          updatePosition(ev.clientX);
+          updatePosition(ev.pageX, ev.pageY);
         });
       }
     };
 
     const handleTouchStart = (ev: TouchEvent): void => {
       if (!isDragging) setIsDragging(true);
-      updatePosition(ev.touches[0].clientX);
+      updatePosition(ev.touches[0].pageX, ev.touches[0].pageY);
     };
 
     const handleTouchMove = (ev: TouchEvent): void => {
       ev.preventDefault();
       if (isDragging) {
         requestAnimationFrame((): void => {
-          updatePosition(ev.touches[0].clientX);
+          updatePosition(ev.touches[0].pageX, ev.touches[0].pageY);
         });
       }
     };
@@ -220,22 +265,27 @@ export const ReactCompareSlider: React.FC<ReactCompareSliderProps> = ({
     };
   }, [isDragging]);
 
+  // Use custom handle if requested
   const Handle = handle || <ReactCompareSliderHandle portrait={portrait} />;
 
   const style: React.CSSProperties = {
     position: 'relative',
     overflow: 'hidden',
-    cursor: isDragging ? 'ew-resize' : undefined,
+    cursor: isDragging ? (portrait ? 'ns-resize' : 'ew-resize') : undefined,
   };
 
   return (
     <div {...props} ref={containerRef} style={style}>
       {itemFirst}
-      <ReactCompareSliderItem position={internalPosition} portrait={portrait}>
+      <ReactCompareSliderItem
+        positionPx={positions.positionPx}
+        position={positions.position}
+        portrait={portrait}
+      >
         {itemSecond}
       </ReactCompareSliderItem>
       <ReactCompareSliderHandleContainer
-        position={internalPosition}
+        position={positions.position}
         portrait={portrait}
       >
         {Handle}
