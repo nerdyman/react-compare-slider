2b20ed64b37c611f38f26e13fa3680d9217a8236
nerdyman <averynerdyman@gmail.com>
2020-03-31 23:06:31 +0100

    build: explicitly define module exports in index to try and appease the tree shaking gods

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,14 @@
-export * from './react-compare-slider-image';
-export * from './react-compare-slider';
+export {
+  ReactCompareSlider,
+  ReactCompareSliderProps,
+  ReactCompareSliderHandle,
+  ReactCompareSliderHandleProps,
+} from './react-compare-slider';
+
+export {
+  CLIENT_SUPPORTS_CSS_OBJECT_FIT,
+  ReactCompareSliderImage,
+  ReactCompareSliderImageProps,
+} from './react-compare-slider-image';
+
 export { styleFitContainer } from './utils';
