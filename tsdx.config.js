9ea26fdbe51c858f0402de374698391707cfbde3
nerdyman <averynerdyman@gmail.com>
2020-12-06 23:15:32 +0000

    perf: add terser plugin for production builds

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,10 +1,15 @@
 /* eslint no-undef: 0, @typescript-eslint/no-var-requires: 0 */
 const analyzer = require('rollup-plugin-analyzer');
+const { terser } = require('rollup-plugin-terser');
 
 module.exports = {
   // This function will run for each entry/format/env combination
-  rollup(config, _options) {
+  rollup(config, options) {
     config.plugins.push(analyzer({ summaryOnly: true, hideDeps: true }));
+
+    if (options.env === 'production') {
+      config.plugins.push(terser());
+    }
 
     return config;
   },
