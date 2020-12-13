af254cf537685b108dada87f8fbc60806305fafc
nerdyman <averynerdyman@gmail.com>
2020-12-13 15:57:43 +0000

    build: update dev deps, add rollup analyzer config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,14 +1,11 @@
 /* eslint no-undef: 0, @typescript-eslint/no-var-requires: 0 */
 const analyzer = require('rollup-plugin-analyzer');
-const { terser } = require('rollup-plugin-terser');
 
 module.exports = {
-  // This function will run for each entry/format/env combination
-  rollup(config, options) {
-    config.plugins.push(analyzer({ summaryOnly: true, hideDeps: true }));
-
-    if (options.env === 'production') {
-      config.plugins.push(terser());
+  // This function will run for each entry/format/env combination.
+  rollup(config /*, options*/) {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(analyzer({ summaryOnly: true, hideDeps: false }));
     }
 
     return config;
