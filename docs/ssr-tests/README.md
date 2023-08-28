e000fd4af3c2ebf7d8883c534ffb226b83741461
nerdyman <averynerdyman@gmail.com>
2023-08-28 11:18:09 +0100

    test(ssr): ensure console warn and error isn't called

---
+++ docs/ssr-tests/README.md
@@ -0,0 +1,9 @@
+# SSR Tests
+
+Super basic test suite to make sure the lib doesn't error server-side.
+
+## Usage
+
+```sh
+pnpm run test
+```
--- docs/ssr-tests/ssr.test.mjs
+++ docs/ssr-tests/ssr.test.mjs
@@ -1,3 +1,5 @@
+// @ts-check
+/* eslint no-console: 0 */
 const assert = await import('node:assert');
 const { describe, it } = await import('node:test');
 
@@ -7,7 +9,10 @@ const { renderToStaticMarkup } = await import('react-dom/server');
 const { ReactCompareSlider, ReactCompareSliderImage } = await import('react-compare-slider');
 
 describe('SSR', () => {
-  it('should render without error', () => {
+  it('should render without error', ({ mock }) => {
+    const mockConsoleError = mock.method(console, 'error');
+    const mockConsoleWarn = mock.method(console, 'warn');
+
     const root = React.createElement(ReactCompareSlider, {
       itemOne: React.createElement(ReactCompareSliderImage, {
         alt: 'Example 1',
@@ -21,7 +26,9 @@ describe('SSR', () => {
 
     assert.strictEqual(React.isValidElement(root), true);
     assert.strictEqual(renderToStaticMarkup(root).includes('data-rcs="root"'), true);
-    assert.strictEqual(renderToStaticMarkup(root).includes('example-1.jpg'), true);
-    assert.strictEqual(renderToStaticMarkup(root).includes('example-2.jpg'), true);
+    assert.strictEqual(renderToStaticMarkup(root).includes('src="example-1.jpg"'), true);
+    assert.strictEqual(renderToStaticMarkup(root).includes('src="example-2.jpg"'), true);
+    assert.strictEqual(mockConsoleError.mock.calls.length, 0);
+    assert.strictEqual(mockConsoleWarn.mock.calls.length, 0);
   });
 });
