b6bb97ff635319b2ea036c9d0deb9f1f01d3a6c0
nerdyman <averynerdyman@gmail.com>
2023-09-15 18:19:52 +0100

    test: add script to change storybook test coverage output json paths to match root `src` dir

--- docs/storybook/.storybook/main.ts
+++ docs/storybook/.storybook/main.ts
@@ -2,6 +2,7 @@
 
 import { resolve } from 'node:path';
 
+import type { AddonOptionsVite } from '@storybook/addon-coverage';
 import type { StorybookConfig } from '@storybook/react-vite';
 import remarkGfm from 'remark-gfm';
 import { mergeConfig } from 'vite';
@@ -10,7 +11,9 @@ const config: StorybookConfig = {
   core: {
     builder: '@storybook/builder-vite',
   },
+
   framework: '@storybook/react-vite',
+
   addons: [
     {
       name: '@storybook/addon-essentials',
@@ -34,19 +37,26 @@ const config: StorybookConfig = {
     '@storybook/addon-coverage',
     '@storybook/addon-storysource',
   ],
+
   stories: ['../content/**/*.stories.@(mdx|tsx)'],
+
   viteFinal: async (config) => {
-    return mergeConfig(config, {
+    const libPath = resolve(__dirname, '..', 'src');
+    console.info(`\n\n[SB CUSTOM] Using lib path: ${libPath}\n\n`);
+
+    const finalConfig = mergeConfig(config, {
       resolve: {
         alias: {
           /**
            * @NOTE This alias is needed for Storybook to correctly generate docs.
            *       Importing from the parent `src` directory does not fully work.
            */
-          'react-compare-slider': resolve(__dirname, '..', 'src'),
+          'react-compare-slider': libPath,
         },
       },
     });
+
+    return finalConfig;
   },
 };
 
