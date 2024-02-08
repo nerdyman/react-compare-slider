0353d7856425727c74b7b94c8ab9e32080215547
nerdyman <averynerdyman@gmail.com>
2024-02-08 16:34:38 +0000

    docs(example): log lib version in example

--- docs/example/src/main.tsx
+++ docs/example/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react';
+import packageJson from 'react-compare-slider/package.json';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 import './index.css';
+
+console.info('react-compare-slider version:', packageJson.version);
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
