b9ec116c0c2c24b1747a890c14fff107ea49ab66
nerdyman <averynerdyman@gmail.com>
2025-12-07 21:02:22 +0000

    build: update storybook imports

--- docs/storybook/.storybook/manager.ts
+++ docs/storybook/.storybook/manager.ts
@@ -1,4 +1,4 @@
-import { addons } from '@storybook/manager-api';
+import { addons } from 'storybook/manager-api';
 import { ThemeVars } from 'storybook/theming';
 import { create } from 'storybook/theming/create';
 
