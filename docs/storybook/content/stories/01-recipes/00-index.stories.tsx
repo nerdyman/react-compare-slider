b79b05f952840d18e2989bb48b4e994722e0ec40
nerdyman <averynerdyman@gmail.com>
2025-12-14 16:58:54 +0000

    docs(storybook): improve autoplay demo

--- docs/storybook/content/stories/01-recipes/00-index.stories.tsx
+++ docs/storybook/content/stories/01-recipes/00-index.stories.tsx
@@ -35,34 +35,40 @@ export const Autoplay: StoryFn<ReactCompareSliderDetailedProps> = (props) => {
   }, [isPlaying]);
 
   return (
-    <div style={{ position: 'relative', width: '100%', padding: '4rem' }}>
-      <ReactCompareSlider
-        {...props}
-        position={sliderPosition}
-        transition={isPlaying ? '3s ease-in-out' : undefined}
-        onPointerDown={() => setIsPlaying(false)}
-        onPointerLeave={() => setIsPlaying(true)}
-        itemOne={
-          <ReactCompareSliderImage
-            src="https://raw.githubusercontent.com/nerdyman/stuff/main/libs/react-compare-slider/demo-images/lady-1.png"
-            alt="Image one"
-          />
-        }
-        itemTwo={
-          <ReactCompareSliderImage
-            src="https://raw.githubusercontent.com/nerdyman/stuff/main/libs/react-compare-slider/demo-images/lady-2.png"
-            alt="Image two"
-            style={{ filter: 'saturate(1.25) contrast(1.1) drop-shadow(2px 4px 6px black)' }}
-          />
-        }
-      />
-    </div>
+    <ReactCompareSlider
+      {...props}
+      position={sliderPosition}
+      transition={isPlaying ? '3s ease-in-out' : undefined}
+      onPointerDown={() => setIsPlaying(false)}
+      onPointerLeave={() => setIsPlaying(true)}
+      itemOne={
+        <ReactCompareSliderImage
+          src="https://raw.githubusercontent.com/nerdyman/stuff/main/libs/react-compare-slider/demo-images/lady-1.png"
+          alt="Image one"
+        />
+      }
+      itemTwo={
+        <ReactCompareSliderImage
+          src="https://raw.githubusercontent.com/nerdyman/stuff/main/libs/react-compare-slider/demo-images/lady-2.png"
+          alt="Image two"
+          style={{ filter: 'saturate(1.25) contrast(1.1) drop-shadow(2px 4px 6px black)' }}
+        />
+      }
+    />
   );
 };
 
 Autoplay.args = {
   style: {
     width: '100%',
+    backgroundColor: 'white',
+    backgroundImage: `
+      linear-gradient(45deg, #ccc 25%, transparent 25%),
+      linear-gradient(-45deg, #ccc 25%, transparent 25%),
+      linear-gradient(45deg, transparent 75%, #ccc 75%),
+      linear-gradient(-45deg, transparent 75%, #ccc 75%)`,
+    backgroundSize: `20px 20px`,
+    backgroundPosition: `0 0, 0 10px, 10px -10px, -10px 0px`,
   },
 };
 
