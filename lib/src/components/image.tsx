dc394f9cc765e141f97360ff199cfd67b5814f4a
nerdyman <averynerdyman@gmail.com>
2026-02-14 14:38:00 +0000

    feat: BREAKING CHANGE set default transition and optimise transition logic

--- lib/src/components/image.tsx
+++ lib/src/components/image.tsx
@@ -7,8 +7,8 @@ import { styleFitContainer } from '../utils';
 export type ImageProps = ComponentProps<'img'>;
 
 /** `img` element with default styles applied by `styleFitContainer`. */
-export const Image: FC<ImageProps> = ({ alt, style, ref, ...props }) => {
+export const Image: FC<ImageProps> = ({ alt, style, ...props }) => {
   const appliedStyle = styleFitContainer(style);
 
-  return <img ref={ref} {...props} alt={alt} style={appliedStyle} data-rcs="image" />;
+  return <img data-rcs="image" {...props} alt={alt} style={appliedStyle} />;
 };
